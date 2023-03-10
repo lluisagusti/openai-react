78cb4cf9f41987c08017a772680884eda4a61faa
Lluis Agusti Sanes <lluis.agusti@melia.com>
2023-03-10 16:31:09 +0100

    openai positive till death

---
+++ src/components/InputArea.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+export default function InputArea({ doAction, setInput, result, input }: any) {
+  return (
+    <div style={{ paddingLeft: '2em', paddingTop: '10em'}}>
+      <div style={{ paddingBottom: '1em'}}>
+      <input className="text-area" width={150} height={1} onChange={(e) => setInput(e.target.value)}></input>
+      <button className="action-btn" onClick={doAction}> POSITIVO </button>
+      </div>
+      <p className="result-text">{result.length > 0 ? `${input[0].toUpperCase()}${input.slice(1)} ${result}` : ""}</p>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,37 @@
 import { Configuration, OpenAIApi } from 'openai'
-import OptionSelection from '../components/OptionSelection'
-import Translation from '../components/Translation'
-import { arrayItems } from '../openai'
+import InputArea from '../components/InputArea'
 import { useState } from 'react'
 import type { NextPage } from 'next'
+
+const chatGPTDavinci3Values = {
+  frequency_penalty: 0,
+  max_tokens: 100,
+  model: "text-davinci-003",
+  presence_penalty: 0,
+  temperature: 0,
+  top_p: 1,
+}
 
 const Home: NextPage = (): JSX.Element => {
   
   const configuration = new Configuration({
     // api key should be in the env file!
     // visit https://platform.openai.com/docs/api-reference/introduction
-    apiKey: '',
+    apiKey: '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',
   })
 
-  const openai = new OpenAIApi(configuration);
-  const [option, setOption] = useState<any>({})
+  const openai = new OpenAIApi(configuration)
+  const option = chatGPTDavinci3Values
   const [result, setResult] = useState<any>('')
   const [input, setInput] = useState<any>('')
 
-  const selectOption = (option: any) => {
-    setOption(option);
-  }
 
   const doAction = async () => {
-    let object: any = { ...option, prompt: input }
+    // Make original and motivational phrases that will inspire others to act and achieve their goals. Original and thought-provoking, these statements ought to compel readers to reflect thoroughly on the strength of their own potential and the opportunities open to them. To deliver your message in a unique and interesting way, think about employing a number of various quotes styles and formats. You should also feel free to try out new words and expressions.
+    
+    const newInput = `Haz una frase original y motivadora que inspirará a otros a actuar y lograr sus objetivos en respuesta a "FRASE_ENTRADA" especificada al final de este texto. Original y estimulante, esta declaración debería obligar a los lectores a reflexionar detenidamente sobre la fortaleza de su propio potencial y las oportunidades que se les abren. Para entregar su mensaje de una manera única e interesante, piense en emplear el formato más persuasivo. También debe sentirse libre de tratar de ser persuasivo. FRASE_ENTRADA: ${input}`
+
+    const object: any = { ...option, prompt: newInput }
 
     const response = await openai.createCompletion(object)
 
@@ -32,11 +40,8 @@ const Home: NextPage = (): JSX.Element => {
 
   return (
     <div className="App">
-      {Object.values(option).length === 0 ? (
-        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
-      ) : (
-        <Translation doAction={doAction} setInput={setInput} result={result} />
-      )}
+      <h3 style={{ textAlign: 'center', paddingTop: '3em' }}>OpenAI Positive Untill death!</h3>
+        <InputArea doAction={doAction} setInput={setInput} result={result} input={input} />
     </div>
   )
 }
